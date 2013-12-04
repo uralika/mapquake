cf6fbf14c429176066194f85415454f41894e5b0
Anastasiya Sutyagina <asutyagina@gmail.com>
2013-12-03 16:35:17 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mapquake::Application.config.secret_key_base = '0f81fc4238806feb15ef3e0cfa25e993e24aa003227a57540dae50b154052db5029b6d17379439a124aaf438d94e3cc6c4c55c8913c4247eb0279c4019daef2b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mapquake::Application.config.session_store :cookie_store, key: '_mapquake_session'
