7de2a74deadec489f0a7b64d1a326f36122be903
Anastasiya Sutyagina <asutyagina@gmail.com>
2013-12-16 15:35:08 -0800

    and more actionmailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,13 +63,15 @@ Mapquake::Application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-   config.action_mailer.delivery_method = :sendmail
-   config.action_mailer.perform_deliveries = true
-   config.action_mailer.raise_delivery_errors = true
-   config.action_mailer.sendmail_settings = {
-  :location => '/usr/sbin/sendmail',
-  :arguments => '-i -t'
-}
+   config.action_mailer.delivery_method = :smtp
+   config.action_mailer.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :domain               => 'mapquake.herokuapp.com',
+  :user_name            => 'mapquake',
+  :password             => 'mapquake123',
+  :authentication       => 'plain',
+  :enable_starttls_auto => true  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
