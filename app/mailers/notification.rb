1fd0da8639e75cc7b0e237dd73aea26090d7e566
Anastasiya Sutyagina <asutyagina@gmail.com>
2013-12-16 18:06:25 -0800

    action mailer upon signup

--- app/mailers/notification.rb
+++ app/mailers/notification.rb
@@ -1,9 +1,12 @@
 class Notification < ActionMailer::Base
-  default from: "mapquake@gmail.com"
+
+
+ default from: "mapquake@gmail.com"
 
   def new_account(user)
   	mail(:to => user.email,
   		 :subject => "The new account #{user.username} is active.")
+
   end
 
 end
