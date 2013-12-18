c300d9a66316e7726ba13c33f1c408864afdf552
Anastasiya Sutyagina <asutyagina@gmail.com>
2013-12-18 14:16:17 -0800

    more sidekiq

--- app/workers/users_worker.rb
+++ app/workers/users_worker.rb
@@ -2,8 +2,9 @@ class UsersWorker
 	include Sidekiq::Worker
 	include Sidetiq::Schedulable
 
-	recurrence { minutely }
+	recurrence { hourly.minute_of_hour(0, 15, 30, 45) }
+
 	def perform(user_id)
 		Notification.daily_quakes(user_id)
-end
+	end
 end
